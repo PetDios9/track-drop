95a10afa8be5161a1f928d352abb7d5a072e3305
Peter Diosdado <diosdadopeter@gmail.com>
2021-11-11 15:28:37 -0800

    initial commit, initiated react and set up folder structure. began making header component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LandingPage from './pages/LandingPage';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AppBar, Toolbar} from "@mui/material"
+
+export default function Header() {
+    return (
+        <div>
+            <AppBar>
+                <Toolbar>
+                    
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
